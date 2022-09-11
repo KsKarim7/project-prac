69cbe0f99cbf1069cd580377933842cff17e037e
Khaled Karim <kskarim177@gmail.com>
2022-09-11 23:41:38 +0600

    drawer added for mobile

--- src/Pages/DrawerNavbar.js
+++ src/Pages/DrawerNavbar.js
@@ -1,9 +1,39 @@
+import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import React from 'react';
+import { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import styled from '@emotion/styled';
+
+const Pages = ["Home", "Notice Board", "Admission", "Contact Us"];
+
+const SideBar = styled(List)`
+background-image:linear-gradient(to bottom, #b22222 11%, #008000 70%);
+color:white
+`
 
 const DrawerNavbar = () => {
+
+    const [openDrawer, setOpenDrawer] = useState(false)
     return (
         <>
+            <Drawer open={openDrawer}
+                onClose={() => setOpenDrawer(false)}>
+                <SideBar>
+                    {
+                        Pages.map((page, index) => (
+                            <ListItemButton onClick={() => setOpenDrawer(false)} key={index}>
+                                <ListItemIcon>
+                                    <ListItemText style={{ color: 'white' }}>{page}</ListItemText>
+                                </ListItemIcon>
+                            </ListItemButton>
 
+                        ))
+                    }
+                </SideBar>
+            </Drawer>
+            <IconButton sx={{ color: 'white', marginLeft: 'auto' }} onClick={() => setOpenDrawer(!openDrawer)}>
+                <MenuIcon></MenuIcon>
+            </IconButton>
         </>
     );
 };
--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,36 +1,74 @@
 import React from 'react';
-import { Typography, AppBar, Toolbar, styled, Tabs, Tab } from '@mui/material';
+import { Typography, AppBar, Toolbar, styled, Tabs, Tab, useMediaQuery, useTheme } from '@mui/material';
 import { useState } from 'react';
+import DrawerNavbar from './DrawerNavbar';
 
-const NavBar = styled(Toolbar)`
+const NavBar = styled(AppBar)`
 background-image:linear-gradient(to left, #b22222 11%, #008000 70%)
 `
 
-const Title = styled(Typography)`
-padding-left:12px;
-font-size:25px;
-font-family:Myriad Pro Semibold
-`
+// const Title = styled(Typography)(({theme})=>({
+//     padding-left:12px,
+//     font-size:25px,
+//     font-family:Myriad Pro Semibold,
+
+// }))
+
+const Title = styled(Typography)(({ theme }) => ({
+    paddingLeft: '12px',
+    fontSize: '25px',
+    fontFamily: 'Myriad Pro Semibold',
+    [theme.breakpoints.down('lg')]: {
+        paddingLeft: '8px',
+        fontSize: '16px',
+        fontFamily: 'Myriad Pro Semibold',
+    }
+
+}));
+
+const Pages = ["Home", "Notice Board", "Admission", "Contact Us"]
 
 const Header = () => {
     const [value, setValue] = useState();
+    const theme = useTheme();
+    const isMatch = useMediaQuery(theme.breakpoints.down('md'))
 
     const logoURL = 'https://i.ibb.co/kyBg7Yc/prasc.png';
     return (
         <>
-            <AppBar>
-                <NavBar>
+            <NavBar>
+                <Toolbar>
                     <img src={logoURL} alt="logo" style={{ width: 95 }} />
-                    <Title>Professor Rowshan Ahmed School and College</Title>
-                    <Tabs sx={{ marginLeft: 'auto' }} value={value} onChange={(e, value) => setValue(value)} textColor="white" indicatorColor="primary">
-                        <Tab label='Home' />
-                        <Tab label='Notice Board' />
-                        <Tab label='Admission' />
-                        <Tab label='Contact Us' />
 
-                    </Tabs>
-                </NavBar>
-            </AppBar>
+                    {
+                        isMatch ? (
+                            <>
+                                <Title>Professor Rowshan Ahmed School and College</Title>
+                                <DrawerNavbar style={{ paddingLeft: '40px' }}></DrawerNavbar>
+                            </>
+                        ) : (
+                            <>
+                                <Title>Professor Rowshan Ahmed School and College</Title>
+                                <Tabs sx={{ marginLeft: 'auto' }} value={value} onChange={(e, value) => setValue(value)} textColor="white" indicatorColor="primary"
+                                >
+                                    {
+                                        Pages.map((page, index) => (
+                                            <Tab key={index} label={page}></Tab>
+                                        ))
+                                    }
+                                    {/* <Tab label='Home' />
+                                    <Tab label='Notice Board' />
+                                    <Tab label='Admission' />
+                                    <Tab label='Contact Us' /> */}
+
+                                </Tabs>
+                            </>
+
+                        )
+                    }
+                </Toolbar>
+
+            </NavBar>
         </>
     );
 };
